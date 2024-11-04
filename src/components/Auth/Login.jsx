d316b0854b0447e17024a170952d80dd48125dfa
mahabub370 <mahabub370@yahoo.com>
2024-11-04 20:21:18 +0600

    take a break

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <div className='flex h-screen w-screen items-center justify-center' >
+      <div className='border-2 border-red-600' >
+        <form action="" className='flex flex-col gap-5'>
+          <input className='text-xl border-2 border-r-sky-700 rounded-3xl py-3 px-4' type="email" placeholder='Enter your email' />
+          <input className='text-xl border-2 border-r-sky-700 rounded-3xl py-3 px-4' type="password" placeholder='Enter your password' />
+          <button className='text-xl border-2 border-r-sky-700 rounded-3xl py-3 px-4'>Login</button>
+        </form>
+      </div>
+    </div>
+    </>
   )
 }
 
